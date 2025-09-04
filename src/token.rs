36d521c82670bed3a9d043e216b14074309272a9
tosh7 <zlia.6.lj.425@gmail.com>
2025-09-05 07:53:54 +0900

    Add token module import and standard library imports

--- src/token.rs
+++ src/token.rs
@@ -1,7 +1,19 @@
-pub struct Token {
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType {
+    Print,
+    Number,
+    LeftParen, // (
+    RightParen, // )
+    EOF,
+}
 
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
 }
 
 impl Token {
-    
+    pub fn new(token_type: TokenType, lexeme: String) -> Self {
+        Token { token_type, lexeme}
+    }
 }
