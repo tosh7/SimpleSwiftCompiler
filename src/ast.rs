96bf5c873a811349942a9fd848a6370c4ccdeeb4
tosh7 <zlia.6.lj.425@gmail.com>
2025-09-08 16:28:13 +0900

    Update AST module implementation

--- src/ast.rs
+++ src/ast.rs
@@ -1,7 +1,27 @@
-pub struct Ast {
-
+#[derive(Debug, Clone, PartialEq)]
+pub enum AstNode {
+    Proguram(Vec<Statement>)
 }
 
-impl Ast {
-    
+#[derive(Debug, Clone, PartialEq)]
+pub enum Statement {
+    Print(Expression),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expression {
+    Number(i64),
+    Binary {
+        left: Box<Expression>,
+        operator: BinaryOperator,
+        right: Box<Expression>,
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BinaryOperator {
+    Add,
+    Subtract,
+    Multiply,
+    Devide,
 }
