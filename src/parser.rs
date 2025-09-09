e607d51ed3df2d082ff1ab26f347cd2f8dadc939
tosh7 <zlia.6.lj.425@gmail.com>
2025-09-09 22:44:24 +0900

    Implement parser module with expression parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,11 @@
-pub struct Parser {
+use create::token::{Token, TokenType};
+use create::ast::{AstNode, Statement, Expression, BinaryOperator};
 
+pub struct Parser {
+    tokens: Vec<Token>,
+    current: usize,
 }
 
 impl Parser {
-    
+
 }
